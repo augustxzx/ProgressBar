2c81ae8dac256108cd1ca8860b45f5adf770fdd4
augustxzx <1753867820@qq.com>
2019-03-25 17:26:44 +0800

    'ProgressBar'

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <progressbar width="100%" leftColor="linear-gradient(to right, blue, green)" bgColor="#ccc"></progressbar>
+  </div>
+</template>
+<script>
+import ProgressBar from './components/ProgressBar.vue'
+export default {     
+  name: 'App',
+  components : {
+    progressbar : ProgressBar  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="progress--wrapper" :style="wrapStyle">
+    <div class="progress" :style="pBarStyle">
+       <div class="left" :style="percentStyle"></div> 
+     </div>   
+  </div>   
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    leftColor : String,
+    bgColor : String,
+    width : String,
+    height : String,
+    percent:{
+      type:Number,
+      default:100 
+    }
+  },
+  data () {
+    return {
+      wrapStyle : {
+        'width' : this.width
+      },
+      pBarStyle : {
+        'width' : this.width,
+        'height' : this.height   
+      },
+      percentStyle : {
+        'width' : this.percent+'%',
+        'height' : this.hegiht,
+        'background' :this.leftColor,
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #progress--wrapper {
+    width : 100%;
+    height : 20px;
+    background-color : #ccc;
+  }
+  .progress {
+    width : 50%;
+    height : 100% ;
+    cursor : point; 
+  }
+  .left {
+    width : 50%;
+    height : 100%;
+    position : relative;  
+  }
+</style>
